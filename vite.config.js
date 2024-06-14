0c02419cac6af250fd2a208f9d2156e65dba6faa
vladius <vladpsico@gmail.com>
2024-06-14 09:33:03 -0600

    Cambio de configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-// import viteDeno from "https://deno.land/x/vite_deno_plugin/mod.ts";
-
+// @type {import('vite').UserConfig}
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
