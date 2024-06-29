9a5c387a22c28a70e5e7e5387397a994db919bbf
vladius <vladpsico@gmail.com>
2024-06-28 18:56:42 -0600

    Se agrega el archivo .env y se modifica el archivo thunks.test.js

--- test/store/auth/thunks.test.js
+++ test/store/auth/thunks.test.js
@@ -1,14 +1,46 @@
-import { describe, expect, test, vi, beforeEach } from "vitest";
-import { ckeckingAuthenticaction } from "../../../src/store/auth/thunks";
-import { checkingCredentials } from "../../../src/store/auth";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import {
+  ckeckingAuthenticaction,
+  startGoogleSignIn,
+} from "../../../src/store/auth/thunks";
+import { checkingCredentials, login, logout } from "../../../src/store/auth";
+import { demoUser } from "../../fixtures/authFixtures";
+import { singInWithGoogle } from "../../../src/firebase/providers/";
+
+vi.mock("../../../src/firebase/providers/");
+
+const dispatch = vi.fn();
+
 
 describe("pruebas en AuthThunks", () => {
   test("debe de invocar el checklnAuthentication", async () => {
-    const dispatch = vi.fn();
-
-    beforeEach( () => vi.mockClear() );
+   
+    beforeEach( () => vi.resetAllMocks() ); 
 
     await ckeckingAuthenticaction()(dispatch);
     expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+  });
+
+  test("startGoogleSignIn debe de llamar chekingCredential y login", async () => {
+    const loginData = { ok: true, ...demoUser };   
+
+    await singInWithGoogle.mockResolvedValue(loginData);
+
+    //Thunk
+
+    await startGoogleSignIn()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(login(loginData));
+  });
+
+  test("startGoogleSignIn debe de llamar al checkCredentials y logout", async() => {
+
+    const loginData = {ok: false, errorMessage: 'un error en google'}
+    await singInWithGoogle.mockResolvedValue(loginData);   
+
+    await startGoogleSignIn()(dispatch);
+    expect(dispatch).toHaveBeenCalledWith(checkingCredentials());
+    expect(dispatch).toHaveBeenCalledWith(logout(loginData.errorMessage));
+
   });
 });
